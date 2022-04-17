42f269593ff3a63f85b685206ec6cee006e06ab4
Harmeet Matharoo <hmjatt@gmail.com>
2022-04-16 19:17:34 -0600

    added Webpack Dev Server

--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,16 @@ module.exports = {
         path: path.resolve(__dirname, "dist"),
         filename: "[name][contenthash].js",
     },
+    devServer: {
+        static: {
+            directory: path.resolve(__dirname, "dist"),
+        },
+        port: 3000,
+        open: true,
+        hot: true,
+        compress: true,
+        historyApiFallback: true,
+    },
     module: {
         rules: [
             {
