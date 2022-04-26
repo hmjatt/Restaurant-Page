7de70c66cc1434503708173f0372adfa0726ce70
Harmeet Matharoo <hmjatt@gmail.com>
2022-04-25 22:33:27 -0600

    page 4 created and styled

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import generatePage1 from "./generatePage1";
 import generatePage2 from "./generatePage2";
 import generatePage3 from "./generatePage3";
+import generatePage4 from "./generatePage4";
 
 
 
-document.addEventListener("load",  generatePage1(), generatePage2(), generatePage3());
+document.addEventListener("load",  generatePage1(), generatePage2(), generatePage3(), generatePage4());
 
 
 
